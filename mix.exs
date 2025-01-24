ac11c80fa41f60141726c959df4ef2c5d0839171
Peter Zingg <peter.zingg@gmail.com>
2025-01-24 12:15:54 -0800

    upgraded dep versions

--- mix.exs
+++ mix.exs
@@ -102,9 +102,9 @@ defmodule Dmp.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.36", only: :dev, runtime: false}
     ]
   end
 end
